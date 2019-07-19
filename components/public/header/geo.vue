50c5b40ef4e6d28a7bc8222ad466ed42a02bea50
gaoxuerong <gaoxuerong@xiaoantech.com>
2019-07-19 23:52:30 +0800

    feat 添加头部

---
+++ components/public/header/geo.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="m-geo">
+  <i class="el-icon-location"/>北京市
+  <nuxt-link class="changeCity" to="/changeCity">切换城市</nuxt-link>
+  [天津 廊坊 邯郸]
+</div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="css">
+
+</style>
+
+
---
+++ components/public/header/index.vue
@@ -0,0 +1,25 @@
+<template>
+<el-row
+  :gutter="15"
+  class="m-header"
+>
+  <el-col :span="6">
+    <geo/>
+  </el-col>
+  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+</el-row>
+</template>
+<script>
+import Geo from './geo.vue'
+export default {
+  components:{
+    Geo
+  }
+}
+</script>
+<style lang="css">
+
+</style>
+
+
